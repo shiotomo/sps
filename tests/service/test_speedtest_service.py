7cc6455ca6e36f0d33cde198ef87bb58be594149
shiotomo <t.shiota16@gmail.com>
2020-04-20 01:57:12 +0900

    speedtest_serviceのテストを追加した

--- tests/service/test_speedtest_service.py
+++ tests/service/test_speedtest_service.py
@@ -1,7 +1,27 @@
 import pytest
 
 from src.service import SpeedtestService
+from src.config import JAPAN_SERVER_LIST
 
 class TestSpeedtestService:
-    def test_aaa(self):
-        assert 1 == 1
+    def setup_method(self, method):
+        self.speedtest_service = SpeedtestService("test")
+        self.server = JAPAN_SERVER_LIST[0]
+        self.error_server_id = 00000
+
+    # run_speedtestの返り値の型を判定する
+    def test_run_speedtest_check_type(self):
+        result = self.speedtest_service.run_speedtest(self.server['id'])
+        assert type(result) is dict
+
+    # run_speedtestの返り値の型を判定する
+    # 存在しないidを入力するため{}が帰ってくる
+    def test_run_speedtest_exception_check_type(self):
+        result = self.speedtest_service.run_speedtest(self.error_server_id)
+        assert type(result) is dict
+
+    # run_speedtestの返り値の型を判定する
+    # 存在しないidを入力するため{}が帰ってくる
+    def test_run_speedtest_check_value(self):
+        result = self.speedtest_service.run_speedtest(self.error_server_id)
+        assert result == {}

4db641703489fe04a48de3c5cbc00817330b3ff8
shiotomo <t.shiota16@gmail.com>
2020-04-25 14:11:21 +0900

    テストコードを追加した

--- tests/util/test_time_utils.py
+++ tests/util/test_time_utils.py
@@ -1,6 +1,6 @@
 import pytest
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 
 from src.util import TimeUtils
@@ -10,9 +10,9 @@ class TestTimeUtils:
         self.datetime_format = "%Y-%m-%d %H:%M:%S"
         self.utc_datetime_format = "%Y-%m-%d %H:%M:%S %z"
 
-    # convert_time_zone_utc_to_jstのテスト
+    # convert_str_datetime_zone_utc_to_jstのテスト
     # UTC(GMT) -> JSTの変換がうまくできているかチェック
-    def test_convert_time_zone_utc_to_jst(self):
+    def test_convert_str_datetime_zone_utc_to_jst(self):
         str_utc_datetime = "Sat, 10 Apr 2020 00:00:00 GMT"
         str_jst_datetime = TimeUtils.convert_str_datetime_timezone_utc_to_jst(str_utc_datetime)
         assert str_jst_datetime == "2020-04-10 09:00:00"
@@ -21,3 +21,12 @@ class TestTimeUtils:
         str_utc_datetime = now.astimezone(timezone("GMT")).strftime(self.utc_datetime_format)
         str_jst_datetime = TimeUtils.convert_str_datetime_timezone_utc_to_jst(str_utc_datetime)
         assert str_jst_datetime == now.astimezone(timezone("Asia/Tokyo")).strftime(self.datetime_format)
+
+    # convert_datetime_zone_utc_to_jstのテスト
+    # UTC(GMT) -> JSTの変換がうまくできているかチェック
+    def test_convert_datetime_zone_utc_to_jst(self):
+        now = datetime.now()
+        hours = timedelta(hours = 9)
+        str_jst_datetime = TimeUtils.convert_datetime_timezone_utc_to_jst(now)
+        # 文字列化してassertする
+        assert str_jst_datetime.strftime(self.datetime_format) == (now + hours).astimezone(timezone("Asia/Tokyo")).strftime(self.datetime_format)
